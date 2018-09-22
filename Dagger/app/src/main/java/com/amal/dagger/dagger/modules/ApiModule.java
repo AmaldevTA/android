6ea4c56464a9fa02ac0ddae58c03acf353fb6bc2
AmaldevTA <ta.amaldev@gmail.com>
2018-09-22 13:00:26 +0530

    dagger added

---
+++ Dagger/app/src/main/java/com/amal/dagger/dagger/modules/ApiModule.java
@@ -0,0 +1,44 @@
+package com.amal.dagger.dagger.modules;
+
+import com.amal.dagger.retrofit.APIComponent;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import javax.inject.Singleton;
+
+import dagger.Module;
+import dagger.Provides;
+import okhttp3.OkHttpClient;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+@Module(includes = OkHttpClientModule.class)
+public class ApiModule {
+
+
+    @Provides
+    public APIComponent randomUsersApi(Retrofit retrofit){
+        return retrofit.create(APIComponent.class);
+    }
+
+    @Provides
+    public Retrofit retrofit(OkHttpClient okHttpClient,
+                             GsonConverterFactory gsonConverterFactory, Gson gson){
+        return new Retrofit.Builder()
+                .client(okHttpClient)
+                .baseUrl("https://randomuser.me/")
+                .addConverterFactory(gsonConverterFactory)
+                .build();
+    }
+
+    @Provides
+    public Gson gson(){
+        GsonBuilder gsonBuilder = new GsonBuilder();
+        return gsonBuilder.create();
+    }
+
+    @Provides
+    public GsonConverterFactory gsonConverterFactory(Gson gson){
+        return GsonConverterFactory.create(gson);
+    }
+}
